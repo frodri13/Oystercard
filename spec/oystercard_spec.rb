ddbbac1c7e4cfaa20010ec66fcb0730664f70728
Fabio <alessf13@gmail.com>
2021-06-01 15:47:41 +0100

    Test for top_up value created and passing. DEFAULT value set to 5.

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,4 +4,9 @@ RSpec.describe Oystercard do
   it 'shows the balance of the card' do
     expect(subject.balance).to eq(0)
   end
+
+  it 'can #top_up into the balance' do 
+    subject.top_up
+    expect(subject.balance).to eq(5)
+  end
 end
